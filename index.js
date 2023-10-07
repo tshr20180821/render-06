b5feab9d11589cf18cbc8640634e663feced4083
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-08 06:04:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const app = express();
+
+const mu = require('./MyUtils.js');
 
 app.get('/', (req, res) => {
   res.send('test');
+  var cls = new mu.MyLog();
 });
 
 app.listen(8080, () => {
