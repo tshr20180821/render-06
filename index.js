861108448facd821ada3a37c1665846f21ec2967
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-08 06:42:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 
 const mu = require('./MyUtils.js');
+const logger = mu.get_logger();
 const myLog = mu.MyLog;
 
 app.get('/', (req, res) => {
   res.send('test');
-  const logger = new myLog();
-  logger.info('INFO message');
+  const mylogger = new myLog();
+  mylogger.info('INFO message');
 });
 
 app.listen(8080, () => {
