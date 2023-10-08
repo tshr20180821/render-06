1d819132b4edc96cecec1cf7d20b4bbf281e0132
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-09 03:26:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const mu = require('./MyUtils.js');
 const logger = mu.get_logger();
 
 app.get('/', (req, res) => {
-  app_get();
+  res.send('test');
+  logger.info('INFO message 2');
 });
 
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
-
-app_get() {
-  res.send('test');
-  logger.info('INFO message 2');
-}
