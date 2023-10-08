aabc9149f2c473b54aa2085de96affce7c6f26fb
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-09 06:17:52 +0900

    Update MyUtils.js

--- MyUtils.js
+++ MyUtils.js
@@ -31,19 +31,17 @@ class MyLog {
   
   #output(level_, message_) {
     try {
-      
-      var e = new Error();
-      var stack = e.stack.split("\n")[3].substring(7);
-      var match = stack.match(this._regex);
-
-      var dt = new Date();
-      var log_header = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' '
-        + ('0' + dt.getHours()).slice(-2) + ':' +  ('0' + dt.getMinutes()).slice(-2) + ':' +  ('0' + dt.getSeconds()).slice(-2) + '.'
-        + ('00' + dt.getMilliseconds()).slice(-3) + ' ' + process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' '
-        + process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
-      console.log(log_header + ' ' + message_);
-      const request = https.request(this._loggly_options);
       new Promise((resolve) => {
+        var e = new Error();
+        var match = e.stack.split("\n")[3].substring(7).match(this._regex);
+        
+        var dt = new Date();
+        var log_header = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' '
+          + ('0' + dt.getHours()).slice(-2) + ':' +  ('0' + dt.getMinutes()).slice(-2) + ':' +  ('0' + dt.getSeconds()).slice(-2) + '.'
+          + ('00' + dt.getMilliseconds()).slice(-3) + ' ' + process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' '
+          + process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
+        console.log(log_header + ' ' + message_);
+        const request = https.request(this._loggly_options);
         request.write(log_header + ' ' + message_);
         request.end();
       });
