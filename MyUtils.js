6ec4d05a570280fbfe0b45cadbcbe7b0491c6444
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-09 03:39:48 +0900

    Update MyUtils.js

--- MyUtils.js
+++ MyUtils.js
@@ -1,23 +1,22 @@
 class MyLog {
   _regex;
   constructor() {
-    this._regex = /.+\/(.+?):(\d+)/;
+    this._regex = .+?at (.+w) .+\/(.+?):(\d+);
   }
   
   info(message_) {
-    this.output(message_);
+    this.output('INFO', message_);
   }
   
   warn(message_) {
     this.output(message_);
   }
   
-  output(message_) {
+  output(level_, message_) {
     console.log(message_);
     try {
       var e = new Error();
       console.log(e.stack);
-      // var level = e.stack.split("\n")[2].substring(7);
       var stack = e.stack.split("\n")[3].substring(7);
       var match = stack.match(this._regex);
       console.log(match);
@@ -26,7 +25,8 @@ class MyLog {
       var log_header = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' '
         + ('0' + dt.getHours()).slice(-2) + ':' +  ('0' + dt.getMinutes()).slice(-2) + ':' +  ('0' + dt.getSeconds()).slice(-2) + '.'
         + ('00' + dt.getMilliseconds()).slice(-3) + ' ' + process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' '
-        + process.pid + ' INFO ' + match[1] + ' ' + match[2] + ' [';
+        + process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
+      console.log(log_header + ' ' + message_);
     } catch (err) {
       console.log(err.toString());
     }
