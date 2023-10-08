2b8817925eb8bc4d08d89f638b7dac3122aba51a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-09 06:22:29 +0900

    Update MyUtils.js

--- MyUtils.js
+++ MyUtils.js
@@ -32,11 +32,10 @@ class MyLog {
   #output(level_, message_) {
     try {
       new Promise((resolve) => {
-        var e = new Error();
-        var match = e.stack.split("\n")[3].substring(7).match(this._regex);
+        const match = (new Error()).stack.split("\n")[3].substring(7).match(this._regex);
         
-        var dt = new Date();
-        var log_header = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' '
+        const dt = new Date();
+        const log_header = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' '
           + ('0' + dt.getHours()).slice(-2) + ':' +  ('0' + dt.getMinutes()).slice(-2) + ':' +  ('0' + dt.getSeconds()).slice(-2) + '.'
           + ('00' + dt.getMilliseconds()).slice(-3) + ' ' + process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' '
           + process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
